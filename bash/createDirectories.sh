0c7b3222dacc4eae9b70b8769e967bcf19c0541c
cameverett <ceverettwhap@gmail.com>
2016-02-04 09:12:14 -0500

    Adding permissions to created directories

--- bash/createDirectories.sh
+++ bash/createDirectories.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-sourceRoot="/home/students"
+sourceRoot="/home"
 group="instructors"
 permissions="770"
 
-# TODO: Parse command line argument passed in classr script --studentfile=<filename>
-while [[ ! -f $studentfile ]]; do
-	read -p "Student File: " studentfile
-done
-
-cat $studentfile | \
+cat "students.data" | \
 while read student; do
 	mkdir -p $sourceRoot/$student/{submit,returned,mynotes}
-	sudo chown -R "$student:$group" $sourceRoot
-	sudo chmod -R $permissions $sourceRoot
+	sudo chown -R "$student:$group" $sourceRoot/$student/{submit,returned,mynotes}
+	sudo chown "$student:$student" $sourceRoot/$student
+	sudo chmod $permissions $sourceRoot/$student/{submit,returned,mynotes}
+
+	if \
+	[ -d $sourceRoot/$student/submit ] && \
+	[ -d $sourceRoot/$student/returned ] && \
+	[ -d $sourceRoot/$student/mynotes ];
+	then
+		echo "Directories created successfully for $student"
+	else
+		>&2 echo "Failed to create directories for $student"
+	fi
 done
+
