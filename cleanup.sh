d8cbd8f0e427b52089c5fc19bb29fb05da9eafeb
cameverett <cameronjeverett@gmail.com>
2016-04-12 14:25:32 +0000

    Personal cleanup script to get rid of IT-R repo. TODO: Remove man pages etc

--- cleanup.sh
+++ cleanup.sh
@@ -1,38 +1,4 @@
 #!/bin/bash
 
-sourceRoot="/home"
+rm -rf "$HOME/itr"
 
-while getopts ":i:s:" opt; do
-	case $opt in
-		i )
-			instructorfile=$OPTARG
-			;;
-		s )
-			studentfile=$OPTARG
-			;;
-		* )
-			printf "Usage sudo </path/to/cleanup.sh> -i </path/to/instructorfile> -s </path/to/studentfile>"
-			;;
-	esac
-done
-
-if [[ ! -f $instructorfile ]]; then
-	printf "\tError '$instructorfile' not a file:\n\tUsage: -i path to instructors file\n"
-else
-	while read instructor; do
-		userdel $instructor -r -f
-	done < $instructorfile
-fi
-
-if [[ ! -f $studentfile ]]; then
-	printf "\tError '$studentfile' not a file:\n\tUsage: -s path to student file\n"
-else
-	
-	while read student; do
-		userdel	$student -r -f
-	done < $studentfile
-fi
-
-groupdel instructors
-rm /usr/share/man/man1/class_collect.1.gz
-rm /usr/share/man/man1/class_create.1.gz
