d40358ebdbf170d1d28a5b67cd086878da190688
cameverett <cameronjeverett@gmail.com>
2016-04-13 16:38:50 +0000

    Updating cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,9 +4,8 @@ echo "Removing ITR Package from your account"
 
 echo "Removing classman alias"
 touch $HOME/temp
-grep -v "alias classman='bash $HOME/itr/classman.sh'" $HOME/.bash_aliases $HOME/temp
+grep -v "alias classman='bash $HOME/itr/classman.sh'" $HOME/.bash_aliases > $HOME/temp
 mv $HOME/temp $HOME/.bash_aliases
-rm $HOME/temp
 
 echo "Removing repo"
 rm -rf "$HOME/itr"
