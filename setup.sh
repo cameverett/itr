d1e21abc195e8e5d5ad1ff3b2537f28d8929c046
cameverett <cameronjeverett@gmail.com>
2016-04-13 15:47:36 +0000

    Leave user grouping to the sys admins. Automatically source the .bash_aliases file

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 
 touch "$HOME/.bash_aliases"
 printf "alias classman='bash $HOME/itr/classman.sh'\n" >> "$HOME/.bash_aliases"
-printf "alias cls='clear && ls'\n" >> "$HOME/.bash_aliases"
 
-sudo usermod -G instructors $1
+source $HOME/.bash_aliases
