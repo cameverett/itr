4b3483f9f175cc9ec4c317c2a76763165bacfe18
cameverett <cameronjeverett@gmail.com>
2016-04-13 14:43:08 +0000

    Removed spurious newlines

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 touch "$HOME/.bash_aliases"
-printf "alias classman=\'bash $HOME/itr/classman.sh\'" >> "$HOME/.bash_aliases\n"
-printf "alias cls=\'clear && ls\'" >> "$HOME/.bash_aliases\n"
+printf "alias classman='bash $HOME/itr/classman.sh'" >> "$HOME/.bash_aliases"
+printf "alias cls='clear && ls'" >> "$HOME/.bash_aliases"
 
 sudo usermod -G instructors $1
