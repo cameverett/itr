c8b8ed57b89b1b9866add7343a73110b12657540
cameverett <cameronjeverett@gmail.com>
2016-04-16 05:16:01 +0000

    fixed issues with adding an option to the -p flag

--- bash/returnHomework.sh
+++ bash/returnHomework.sh
@@ -5,39 +5,37 @@ permissions="770"
 
 while getopts ":i:s:p:f:" opt; do
 	case $opt in
-	i ) instructor=$OPTARG;;
-	s ) studentfile=$OPTARG;;
-	p ) path=$OPTARG;;
-	f ) flag=$OPTARG;;
+	i ) instructor=$OPTARG ;;
+	s ) studentfile=$OPTARG ;;
+	p ) pathExtension=$OPTARG ;;
+	f ) flag=$OPTARG ;;
 	* ) printf "Usage classman return -s </path/to/studentfile> -i <your_username> -f <flag> -p </path/in/your/homework/directory\n"
 	esac
 done
-	
-if [[ ! $instructor ]] || [[ ! -f "$studentfile" ]]; then
-	if [ ! $instructor ]; then
-		printf "Missing Argument -i <instructor_username>"
-	elif [ ! -f "$studentfile" ]; then
+
+if [[ ! $instructor ]] || [[ ! -f $studentfile ]]; then
+	if [[ ! $instructor ]]; then
+		printf "Missing Argument -i <instructor_username>\n"
+	elif [[ ! -f $studentfile ]]; then
 		printf "Missing Argument -s </path/to/studentfile>\n"
 	fi
 	exit 1
 fi	
 
-if [ $path ]; then
-	path="$sourceRoot/$instructor/homework/$path"
+pathRoot="$sourceRoot/$instructor/homework"
+if [[ $pathExtension ]]; then
+	pathExtension="$pathExtension/"
 else
-	path="$sourceRoot/$instructor/homework"
-fi
-
-
-
-if [ ! -d "$path/" ]; then
-	printf "%s does not exist." "$path/"
-	exit 1
+	pathExtension=""
 fi
 
 while read student; do	
-	cp $path/$student/$flag* "$destinationRoot/$student/returned" 2> /dev/null
-	sudo chown -R "$student:$group" "$destinationRoot/$student/returned"
-	sudo chmod -R "$permissions" "$destinationRoot/$student/returned"
+	if [[ ! -d "$pathRoot/$student/$pathExtension" ]]; then
+		printf "%s/%s/%s/ does not exist.\n" "$pathRoot" "$student" "$pathExtension"
+	else
+		cp $pathRoot/$student/$pathExtension$flag* "$destinationRoot/$student/returned" 2> /dev/null
+		sudo chown -R "$student:$group" "$destinationRoot/$student/returned"
+		sudo chmod -R "$permissions" "$destinationRoot/$student/returned"
+	fi
 done < $studentfile
 
