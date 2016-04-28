6384508dcad7982d1660d9ba604cdc4141002d30
cameverett <cameronjeverett@gmail.com>
2016-04-28 15:55:07 +0000

    Needed to place quotes around path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 touch "$HOME/.bash_aliases"
 printf "alias classman='bash $HOME/itr/classman.sh'\n" >> "$HOME/.bash_aliases"
 
-source $HOME/.bash_aliases
+source "$HOME/.bash_aliases"
