bfe8676e46c7f2958063415aece44ff787113591
cameverett <cameronjeverett@gmail.com>
2016-05-28 07:56:53 +0000

    Searching for patterns anywhere in filename

--- bash/collectHomework.sh
+++ bash/collectHomework.sh
@@ -53,9 +53,10 @@ fi
 
 touch "$destination/log.tsv"
 while read student; do
-	if ls -U $sourceRoot/$student/submit/$tag* 1> /dev/null 2>&1; then
+	#if ls -U $sourceRoot/$student/submit/$tag* 1> /dev/null 2>&1; then
+	if find "$sourceRoot/$student/submit/"*$tag* 1> /dev/null 2>&1; then
 		mkdir -p "$destination/$student"
-		cp $sourceRoot/$student/submit/$tag* "$destination/$student"
+		cp "$sourceRoot/$student/submit/"*$tag* "$destination/$student"
 	else
 		printf "%s\t%s\n" "$student" "$tag" >> "$sourceRoot/$instructor/homework/log.tsv"
 	fi
