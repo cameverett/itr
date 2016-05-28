fdef932f0402c8ac9efb9859d5848828bf3bdb68
cameverett <cameronjeverett@gmail.com>
2016-05-28 15:32:26 +0000

    appending names to file for now

--- bash/collectHomework.sh
+++ bash/collectHomework.sh
@@ -55,12 +55,11 @@ fi
 
 touch "$destination/log.tsv"
 while read student; do
-	if ls -U $sourceRoot/$student/submit/$tag* 1> /dev/null 2>&1; then
-	#if find "$sourceRoot/$student/submit/"*$tag* 1> /dev/null 2>&1; then
+	if find "$sourceRoot/$student/submit/"*$tag* 1> /dev/null 2>&1; then
 		mkdir -p "$destination/$student"
 		cp "$sourceRoot/$student/submit/"*$tag* "$destination/$student"
 	else
-		printf "%s\t%s\n" "$student" "$tag" > "$sourceRoot/$instructor/homework/"$tag"log.tsv"
+		printf "%s\t%s\n" "$student" "$tag" >> "$sourceRoot/$instructor/homework/"$tag"log.tsv"
 	fi
 done < "$studentfile"
 
