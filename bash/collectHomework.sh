ea710f8b16e4e3d7d613755bc59e0f8d476ee236
cameverett <cameronjeverett@gmail.com>
2016-06-10 04:40:13 +0000

    copy a homework folder not just a file

--- bash/collectHomework.sh
+++ bash/collectHomework.sh
@@ -59,7 +59,7 @@ printf "Logged at: %s\n" "$timeCollected" >> "$destination/"$tag"log"
 while read student; do
 	if find "$sourceRoot/$student/submit/"*$tag* 1> /dev/null 2>&1; then
 		mkdir -p "$destination/$student"
-		cp "$sourceRoot/$student/submit/"*$tag* "$destination/$student"
+		cp -r "$sourceRoot/$student/submit/"*$tag* "$destination/$student"
 	else
 		printf "%s\t%s\n" "$student" "$tag" >> "$destination/"$tag"log"
 	fi
